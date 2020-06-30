fbd79f2c46f27ab310199985a10f0dd65ac731fa
Anatoly Kobzisty <anatolykobzisty@gmail.com>
2020-06-30 19:26:55 +0600

    Типизация функциональных компонентов

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,31 @@
 import React from 'react';
 
-const App = () => (
-  <h1>Hello world!</h1>
+/*
+  1 способ
+
+const Title: React.FC<{title: string}> = ({ title, children }) => (
+  <h1>
+    {title}
+    {children}
+  </h1>
 );
+
+*/
+
+// 2 способ
+
+type TitleProps = {
+  title: string,
+  test?: string, // необязательный параметр
+};
+
+const Title = ({ title, test = 'test' }: TitleProps) => (
+  <h1>
+    {title}
+    {test}
+  </h1>
+);
+
+const App = () => <Title title="test" />;
 
 export default App;
