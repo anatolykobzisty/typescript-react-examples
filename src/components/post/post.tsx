d1e76da11eadf6f0f7e9821db22c5dc4c1cc9247
Anatoly Kobzisty <anatolykobzisty@gmail.com>
2020-07-12 22:34:54 +0300

    Типизация асинхронных функций

--- src/components/post/post.tsx
+++ src/components/post/post.tsx
@@ -1,6 +1,49 @@
 import React, { Component } from 'react';
 import { RouteComponentProps } from 'react-router-dom';
 
+/*
+1 СЛУЧАЙ
+
+// Example of full response:
+interface HttpResponse<T> extends Response {
+  parsedBody?: T;
+  status: number,
+  redirect: boolean,
+  ...
+}
+
+export async function http<T>(request: string): Promise<HttpResponse<T>> {
+  const response: HttpResponse<T> = await fetch(request);
+  response.parsedBody = await response.json();
+  return response;
+}
+
+// Example of errors handling
+export async function http<T>(request: string): Promise<HttpResponse<T>> {
+  const response: HttpResponse<T> = await fetch(request);
+  try {
+    // Error if there is no body
+    response.parsedBody = await response.json();
+  } catch (ex) {}
+  if (!response.ok) {
+    // Error if there is response status issue
+    throw new Error(response.statusText);
+  }
+  return response;
+}
+
+// Using:
+try {
+  const resp = await http<IPost>(`https://jsonplaceholder.typicode.com/posts/${id}`);
+  console.log("response", resp);
+} catch (resp) {
+  console.log("Error", resp);
+}
+
+*/
+
+// 2 СЛУЧАЙ
+
 type RouteParams = {
   id: string,
 };
@@ -14,6 +57,12 @@ type PostState = {
   post: IPost,
 };
 
+export async function http<T>(reques: string): Promise<T> {
+  const response = await fetch(reques);
+  const body = await response.json();
+  return body;
+}
+
 class Post extends Component<RouteComponentProps<RouteParams>, PostState> {
   state = {
     post: {
@@ -22,16 +71,12 @@ class Post extends Component<RouteComponentProps<RouteParams>, PostState> {
     },
   };
 
-  componentDidMount() {
+  async componentDidMount() {
     const { match } = this.props;
     const id = match.params.id || '';
 
-    fetch(`https://jsonplaceholder.typicode.com/posts/${id}`)
-      .then((res) => res.json())
-      .then((post) => { this.setState({ post }); })
-      .catch((error) => {
-        console.log(error);
-      });
+    const post = await http<IPost>(`https://jsonplaceholder.typicode.com/posts/${id}`);
+    this.setState({ post });
   }
 
   render() {
