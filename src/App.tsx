587ccbcb95cb52399c789fb763ba262f8bba75a6
Anatoly Kobzisty <anatolykobzisty@gmail.com>
2020-07-12 18:03:05 +0300

    Типизация Роутера

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,14 @@
-import React, { useState } from 'react';
+import React from 'react';
 
-type BaseProps = {
-  primTitle: string,
-  secTitle?: string,
-};
+import Header from './components/header/header';
+import Footer from './components/footer/footer';
 
-type InjectedProps = {
-  toggleStatus: boolean,
-  toggle: () => void,
-};
-
-const Button = ({
-  primTitle, secTitle, toggle, toggleStatus,
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-}: any) => (
-  <button type="button" onClick={toggle}>
-    {toggleStatus ? primTitle : secTitle}
-  </button>
+const App:React.FC = ({ children }) => (
+  <>
+    <Header />
+    {children}
+    <Footer />
+  </>
 );
-
-const withToggle = <BaseProps extends InjectedProps>(PassedComponent: React.ComponentType<BaseProps>) => (props: BaseProps) => {
-  const [toggleStatus, toggle] = useState(false);
-
-  return (
-    <PassedComponent
-      {...props} // типизация остаточных аргументов
-      toggle={() => toggle(!toggleStatus)}
-      toggleStatus={toggleStatus}
-    />
-  );
-};
-
-const ToogleButton = withToggle(Button);
-
-const App:React.FC = () => <ToogleButton primTitle="Maint Title" secTitle="Additional Title" />;
 
 export default App;
---
+++ src/components/posts/posts.tsx
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import { Link, RouteComponentProps } from 'react-router-dom';
+
+
+interface IPost {
+  title?: string,
+  id?: number,
+}
+
+type PostState = {
+  data: IPost[],
+};
+
+interface IPosts extends RouteComponentProps {
+  test?: number,
+}
+
+class Posts extends Component<IPosts, PostState> {
+  state = {
+    data: [],
+  };
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/posts/')
+      .then((res) => res.json())
+      .then((data) => { this.setState({ data }); })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  render() {
+    const { data } = this.state;
+    const { match } = this.props;
+    console.log(match.params);
+
+    return (
+      <div>
+        <h1>Posts:</h1>
+        <ul className="posts">
+          {data.map(({ id, title }: IPost) => <li key={id}><Link to={`/posts/${id || 'zero'}`}>{title}</Link></li>)}
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default Posts;
