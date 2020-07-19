acce945a2541c9bb985a26f4f017e585313ae58b
Anatoly Kobzisty <anatolykobzisty@gmail.com>
2020-07-19 15:16:52 +0300

    add rule Eslint

--- src/App.tsx
+++ src/App.tsx
@@ -54,6 +54,68 @@ class MyComponent extends Component<{}, { count: number }> {
   }
 }
 
-const App:React.FC = () => <MyComponent />;
+interface IContext {
+  isAuth: Boolean,
+  toggleAuth: () => void,
+}
+
+// Context creation
+const AuthContext = React.createContext<IContext>({
+  isAuth: false,
+  toggleAuth: () => {},
+});
+
+// Внутренний компонент  (new syntax of static property)
+class Login extends Component {
+
+  static contextType = AuthContext;
+  context!: React.ContextType<typeof AuthContext>
+
+  render() {
+    const { toggleAuth, isAuth } = this.context;
+
+    return (
+      <button onClick={toggleAuth}>
+        {!isAuth ? 'Login' : 'Logout'}
+      </button>
+    );
+  }
+}
+
+// Внутренний компонент (old variant with Consumer)
+const Profile: React.FC = (): React.ReactElement => (
+  <AuthContext.Consumer>
+    {({ isAuth }: IContext) => (
+      <h1>{!isAuth ? 'Please log in' : 'You are logged in'}</h1>
+    )}
+  </AuthContext.Consumer>
+);
+
+// Корневой компонент
+class Context extends Component<{}, { isAuth: boolean }> {
+  readonly state = {
+    isAuth: false,
+  };
+
+  toggleAuth = () => {
+    this.setState(({ isAuth }) => ({
+      isAuth: !isAuth,
+    }));
+  };
+
+  render() {
+    const { isAuth } = this.state;
+    const context: IContext = { isAuth, toggleAuth: this.toggleAuth };
+
+    return (
+      <AuthContext.Provider value={context}>
+        <Login />
+        <Profile />
+      </AuthContext.Provider>
+    );
+  }
+}
+
+const App:React.FC = () => <Context />;
 
 export default App;
